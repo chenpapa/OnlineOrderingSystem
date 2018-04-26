8b6a4971ceb4201aff5231d159a9ceb7a2266443
chenpapapa <529953602@qq.com>
2018-04-26 12:33:43 +0800

    0.0.2

--- src/main/java/la/chopper/web/OrderController.java
+++ src/main/java/la/chopper/web/OrderController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("order")
+@RequestMapping("/order")
 public class OrderController extends BaseController {
 
     private OrderService orderService;
